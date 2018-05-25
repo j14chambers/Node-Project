053464d764bea64b38fe1f43e83d50ad64097fc4
j14chambers <jessica14chambers@gmail.com>
2018-05-24 19:43:43 -0500

    Update file to navigate to article page

--- nodekb/app.js
+++ nodekb/app.js
@@ -58,6 +58,17 @@ app.get('/', function(req, res){
     });
 });
 
+//Get Single Article
+app.get('/article/:id', function(req, res){
+    Article.findById(req.params.id, function(err, article){
+        res.render('article', {
+            article:article
+        });
+        //console.log(article);
+        //return;
+    });
+});
+
 //Add Route
 app.get('/articles/add', function(req, res) {
     res.render('add_article', {
