c93c76060fb63bd6135034fd761c62cf1c6b6e64
j14chambers <jessica14chambers@gmail.com>
2018-06-02 20:07:27 -0500

    install body-parser and add 'add article to database'

--- nodekb/app.js
+++ nodekb/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -24,6 +26,12 @@ let Article = require('./models/article');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//Body Parser Middleware
+app.use(bodyParser.urlencoded({extended: false}));
+//parse application/json
+app.use(bodyParser.json());
+
+
 //Home route
 app.get('/', function(req, res){
     Article.find({}, function(err, articles){
@@ -45,6 +53,26 @@ app.get('/articles/add', function(req,res){
     res.render('add_article',{
         title:'Add Article'
     });
+});
+
+//add Submit POST Route
+app.post('/articles/add', function(req,res){
+    //console.log('Submitted');
+    let article = new Article();
+    article.title = req.body.title;
+    article.author = req.body.author;
+    article.body = req.body.body;
+   // console.log(req.body.title);
+
+   article.save(function(err){
+       if(err){
+           console.log(err);
+           return;
+       }
+       else{
+           res.redirect('/');
+       }
+   });
 });
 
 //Start Sever
